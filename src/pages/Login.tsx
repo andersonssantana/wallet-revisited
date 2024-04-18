d562651e70e1d3c123d1f067811bea619ea50f22
Anderson Santana <anderslash@gmail.com>
2024-04-18 16:09:08 -0300

    validate login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,38 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as EmailValidator from 'email-validator';
+
 export function Login() {
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    navigate('/carteira');
+  };
+
+  const validateEmail = ():boolean => {
+    const valid = EmailValidator.validate(email);
+    return !valid;
+  };
+
   return (
-    <div>
-      Login Page
-    </div>
+    <>
+      <div>
+        Login Page
+      </div>
+      <input
+        type="email"
+        id="email"
+        onChange={ (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value) }
+      />
+      <button
+        type="submit"
+        onClick={ handleLogin }
+        disabled={ validateEmail() }
+      >
+        Entrar
+      </button>
+    </>
   );
 }
