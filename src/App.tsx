008c78fa2964863f0fe85a609b5c7dcc6d04a70d
Anderson Santana <anderslash@gmail.com>
2024-04-18 15:42:14 -0300

    add Page Not Found

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Wallet } from './pages/Wallet';
+import { NotFound } from './pages/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
       <Route path="/carteira" element={ <Wallet /> } />
+      <Route path="*" element={ <NotFound /> } />
     </Routes>
   );
 }
