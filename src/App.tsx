99affbdec950af2252ddc782a2f2ef95f3ae6b86
Anderson Santana <anderslash@gmail.com>
2024-04-18 15:36:34 -0300

    add basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { Wallet } from './pages/Wallet';
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/carteira" element={ <Wallet /> } />
+      </Routes>
+    </Router>
   );
 }
 
