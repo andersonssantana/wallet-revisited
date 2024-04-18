47f32a70f33942377c72217dc4a277771ea6bfb3
Anderson Santana <anderslash@gmail.com>
2024-04-18 15:39:24 -0300

    move router to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Wallet } from './pages/Wallet';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={ <Login /> } />
-        <Route path="/carteira" element={ <Wallet /> } />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+      <Route path="/carteira" element={ <Wallet /> } />
+    </Routes>
   );
 }
 
